4a2d21181a934999d3b857dd34fee5cc775e082f
Villy-P <valeriuspetrini@gmail.com>
2023-07-25 09:31:04 -0500

    Add Windows Support

--- src/exec.c
+++ src/exec.c
@@ -1,30 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/wait.h>
 
 #include "exec.h"
+
+#ifdef _WIN32
+#include <windows.h>
+#elif linux
 #include "unistd.h"
+#include <sys/wait.h>
+#endif
 
 void execute(char* command) {
-    __pid_t pid = fork();
-    if (pid < 0) {
-        printf("fork() caused an error.");
-        exit(1);
-    } else if (pid == 0) {
+    #ifdef _WIN32
+        STARTUPINFO si;
+        PROCESS_INFORMATION pi;
+
+        ZeroMemory(&si, sizeof(STARTUPINFO));
+        si.cb = sizeof(STARTUPINFO);
+        ZeroMemory(&pi, sizeof(PROCESS_INFORMATION));
+
         if (strncmp(command, "cd ", 3) == 0) {
             command += 3;
-            chdir(command);
+            if (!SetCurrentDirectoryA(command)) {
+                printf("The system cannot find the path specified.\n");
+                return;
+            }
         } else {
-            char* token = strtok(command, " ");
-            char* cmd;
-            strcpy(cmd, token);
-            token = strtok(NULL, " ");
-            execlp(cmd, cmd, token, (char *)NULL);
-            perror("");
+            char* runcmd = "cmd /c ";
+            size_t runcmdLen = strlen(runcmd);
+            size_t commandLen = strlen(command);
+            char* cmd = (char*)malloc(runcmdLen + commandLen + 1);
+
+            strcpy(cmd, runcmd);
+            strcat(cmd, command);
+            if (!CreateProcessA(NULL, cmd, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi)) {
+                printf("Failed to execute command. (Are you running as an administrator?)\n");
+                return;
+            }
+            WaitForSingleObject(pi.hProcess, INFINITE);
+            CloseHandle(pi.hProcess);
+            CloseHandle(pi.hThread);
         }
-    } else {
-        int childStatus;
-        wait(&childStatus);
-    }
+    #elif linux
+        __pid_t pid = fork();
+        if (pid < 0) {
+            printf("fork() caused an error.");
+            exit(1);
+        } else if (pid == 0) {
+            if (strncmp(command, "cd ", 3) == 0) {
+                command += 3;
+                chdir(command);
+            } else {
+                char* token = strtok(command, " ");
+                char* cmd;
+                strcpy(cmd, token);
+                token = strtok(NULL, " ");
+                execlp(cmd, cmd, token, (char *)NULL);
+                perror("");
+            }
+        } else {
+            int childStatus;
+            wait(&childStatus);
+        }
+    #endif
 }
--- src/main.c
+++ src/main.c
@@ -2,14 +2,23 @@
 #include <stdbool.h>
 
 #include "exec.h"
+
+#ifdef _WIN32
+#include <windows.h>
+#elif linux
 #include "unistd.h"
+#endif
 
 #define LINE_BUFFER_SIZE 255
 
 int main() {
     while (true) {
         char cwd[4096];
-        getcwd(cwd, sizeof(cwd));
+        #ifdef _WIN32
+            GetCurrentDirectory(sizeof(cwd), cwd);
+        #else
+            getcwd(cwd, sizeof(cwd));
+        #endif
         char input[LINE_BUFFER_SIZE];
         printf("%s >>>", cwd);
         fgets(input, LINE_BUFFER_SIZE, stdin);
